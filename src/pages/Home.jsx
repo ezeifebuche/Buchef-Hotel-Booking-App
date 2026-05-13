40e159a5d1541465f4356b9065b081bcc6fed99e
Goodboy <emzzyeze8905216@gmail.com>
2026-05-13 17:51:55 +0100

    fix home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '../Components/Hero'
-import FeaturedDestination from '../Components/featuredDestination'
+import FeaturedDestination from '../Components/FeaturedDestination'
 import ExclusiveOffer from '../Components/ExclusiveOffer'
 import Testimonial from '../Components/Testimonial'
 import NewsLetter from '../Components/NewsLetter'
